8157947f190e49372fc4c1f8572e4d2dbca12413
Daniel Diaz <dddiaz@uci.edu>
2015-03-25 20:29:45 -0700

    making amazon s3 sync class

--- prometheus.py
+++ prometheus.py
@@ -1,13 +1,53 @@
-#Project Prometheus Cron Job
-#By: Daniel Diaz 2015
-#Purpose: To periodically sync with amazon services
+#Project:	Prometheus Cron Job
+#By: 		Daniel Diaz 2015
+#			www.danieldelvindiaz.com
+#Purpose: 	To periodically sync with amazon services
+#Note: 		You must add the cron job call in /etc/cron.d
+#Python-V: 	3 (Must be called with python3 in Rasberry Pi instead of deafault 2.x)
+#TODO:		-test cron job exists
+#			-test python installed
+#			-test python installer installed
+#			-test awscli installed/setup
+#			-google cli????
+#			-run aws sync
 
-#Note you must add the cron job call in /etc/cron.d
 
-#testing python cron job
-import datetime
-f = open('/home/pi/cron.txt','a')
-f.write('test \n')
-f.write(datetime.datetime.now().strftime("%B %d, %Y"))
-f.write('\n')
-f.close()
+class amazonS3Sync:
+	"""
+		Implements a class to sync with amazon amazonS3
+	"""
+	def __init__(self):
+		"""
+			__init__
+		"""
+		self.complete = false
+
+	def printSyncStart(self):
+		"""
+			Print to cron.txt a start sync time
+		"""
+		f = open('/home/pi/cron.txt','a')
+		f.write('Starting sync: ')
+		f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		f.write('\n')
+		f.close()
+
+	def syncToS3(self):
+		"""
+			Sync to aws s3 with awscli
+		"""
+		z=1
+
+	def execute(self):
+		self.printSyncStart()
+		self.syncToS3()
+
+if '__name__' == '__main__':
+	import datetime
+	backup = amazonS3Sync()
+	backup.execute()
+
+
+
+
+
