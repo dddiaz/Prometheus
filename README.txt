f90cb8520d748f26d041d19b79051b41c7ebb6e5
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2015-06-08 23:21:26 -0700

    coming back to project, setting up in pycharm

--- README.txt
+++ README.txt
@@ -4,3 +4,18 @@ Project Prometheus is a Raspberry Pi 2 project.
 
 Concept:
 	Create a solution to automatically back up external drives to amazon webservices (S3 and glacier).
+	Will be possible by deploying this python3 app to the raspberry pi, and running it through the command line.
+
+Dev Notes:
+#Project:	Prometheus Cron Job
+#By: 		Daniel Diaz 2015
+#			www.danieldelvindiaz.com
+#Purpose: 	To periodically sync with amazon services
+#Note: 		You must add the cron job call in /etc/cron.d
+#Python-V: 	3 (Must be called with python3 in Rasberry Pi instead of deafault 2.x)
+#TODO:		-test cron job exists
+#			-test python installed
+#			-test python installer installed
+#			-test awscli installed/setup
+#			-google cli????
+#			-run aws sync
---
+++ prometheus-old.py
@@ -0,0 +1,55 @@
+#Project:	Prometheus Cron Job
+#By: 		Daniel Diaz 2015
+#Web:		www.danieldelvindiaz.com
+#Purpose: 	To periodically sync with amazon services
+#Note: 		You must add the cron job call in /etc/cron.d
+#Python-V: 	3 (Must be called with python3 in Rasberry Pi instead of deafault 2.x)
+
+
+import datetime
+import subprocess
+
+class amazonS3Sync:
+	"""
+		Implements a class to sync with amazon amazonS3
+	"""
+	def __init__(self):
+		"""
+			__init__
+		"""
+		self.complete = False
+
+	def printSyncStart(self):
+		"""
+			Print to cron.txt a start sync time
+		"""
+		f = open('/home/pi/cron.txt','a')
+		f.write('Starting sync: ')
+		f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		f.write('\n')
+		f.close()
+
+	def syncToS3(self):
+		"""
+			Sync to aws s3 with awscli
+			subprocess.call will wait for finish which we should do
+		"""
+		#return_code = subprocess.call("echo Hello World", shell=True)
+		return_code = subprocess.call("aws s3 ls s3://prometheus-bucket-raspberry-pi",shell=True)
+		return_code1 = subprocess.call("aws s3 sync /mnt/usb s3://prometheus-bucket-raspberry-pi",shell=True)
+		return_code2 = subprocess.call("aws s3 ls s3://prometheus-bucket-raspberry-pi",shell=True)
+		print("Return Code", return_code,return_code1,return_code2)
+
+	def execute(self):
+		self.printSyncStart()
+		self.syncToS3()
+
+if __name__ == '__main__':
+	print("Welcome To Prometheus\n")
+    #print("An AWS Sync app by www.danieldelvindiaz.com")
+    #backup = amazonS3Sync()
+	#backup.execute()
+
+
+
+
